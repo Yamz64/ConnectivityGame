ef7e482b9c62247ece490d47ab1ebd367ed679e9
James Lillicrap <jamesplillicrap@gmail.com>
2020-09-23 16:26:44 -0400

    +Implemented Base Movement Class and Started Work on Big One Behavior
    +Created a base class for movement
    +Added support for derived classes for big one and little one

---
+++ ConnectivityGame/Assets/Scripts/BigOneBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigOneBehavior : CharacterMovement
+{
+    private Vector3 held_object_transform;      //position of held object starting the game
+    private GameObject held_object_position;    //current position of held object
+
+    new void Start()
+    {
+        base.Start();
+        held_object_position = transform.GetChild(0).gameObject;
+        held_object_transform = held_object_position.transform.localPosition;
+    }
+
+    new void Update()
+    {
+        base.Update();
+        //handle flipping the held object
+        if (flipped)
+        {
+            held_object_position.transform.localPosition = new Vector3(-held_object_transform.x, held_object_transform.y, held_object_transform.z);
+        }
+        else
+        {
+            held_object_position.transform.localPosition = new Vector3(held_object_transform.x, held_object_transform.y, held_object_transform.z);
+        }
+    }
+}
---
+++ ConnectivityGame/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float move_speed;
+    public float jump_speed;
+    public float jump_timer;
+    public float gravity_scale;
+    public bool grounded = false;
+    public bool flipped;
+
+    private float max_jump_timer;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    //function handles jumping and vertical movement
+    public void JumpFunc()
+    {
+        if (grounded)
+        {
+            jump_timer = max_jump_timer;
+        }
+
+        //jump is inputted
+        if (Input.GetAxis("Vertical") > 0.0f || Input.GetAxis("Jump") > 0.0f)
+        {
+            if (jump_timer > 0.0f)
+            {
+                jump_timer -= 1.0f * Time.deltaTime;
+                rb.velocity = new Vector2(rb.velocity.x, jump_speed);
+            }
+        }
+        //Jump is released
+        else if (Input.GetAxis("Vertical") == 0.0f && Input.GetAxis("Jump") == 0.0f)
+        {
+            if (!grounded)
+            {
+                jump_timer = 0.0f;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        max_jump_timer = jump_timer;
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = gravity_scale;
+
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //--MOVEMENT--
+        //--handle vertical movement--
+        JumpFunc();
+
+        //--handle horizontal movement--
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * move_speed, rb.velocity.y);
+
+        //--VISUAL--
+        //--handle sprite flipping
+        if(Input.GetAxis("Horizontal") > 0.0f)
+        {
+            flipped = false;
+        }
+        else if(Input.GetAxis("Horizontal") < 0.0f)
+        {
+            flipped = true;
+        }
+        sprite.flipX = flipped;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        grounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        grounded = false;
+    }
+}
