bb260bbd54b3ecb56bd69d4bcb5f3d9df470b137
James Lillicrap <jamesplillicrap@gmail.com>
2020-09-28 18:36:26 -0400

    +Fixed little one not being able to escape

--- ConnectivityGame/Assets/Scripts/BigOneBehavior.cs
+++ ConnectivityGame/Assets/Scripts/BigOneBehavior.cs
@@ -301,7 +301,7 @@ public class BigOneBehavior : CharacterMovement
 
             ThrowHandler();
             Animate();
-            if (stop_grab) { EscapeHandler(); }
         }
+        if (stop_grab) { EscapeHandler(); }
     }
 }
