a46efbbb9546bf2a5ee2c3adb2e36612ec338950
James Lillicrap <jamesplillicrap@gmail.com>
2020-09-29 03:32:50 -0400

    +Added all sounds, implemented victory sound when chalk is collected

--- ConnectivityGame/Assets/Scripts/GoalBehavior.cs
+++ ConnectivityGame/Assets/Scripts/GoalBehavior.cs
@@ -5,21 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class GoalBehavior : MonoBehaviour
 {
+    public bool bang;
+
     public string next_scene_name;
 
     private bool little_one;
     private bool big_one;
 
+    private AudioSource victory_theme;
+
+    IEnumerator Victory_Sequence()
+    {
+        victory_theme.Play();
+        yield return new WaitForSeconds(victory_theme.clip.length);
+        if (next_scene_name != "")
+        {
+            SceneManager.LoadScene(next_scene_name);
+        }
+        else
+        {
+            Debug.Log("No scene name");
+        }
+    }
+
+    private void Start()
+    {
+        victory_theme = GetComponent<AudioSource>();
+    }
+
     private void Update()
     {
         if(little_one && big_one)
         {
-            if (next_scene_name != ""){
-                SceneManager.LoadScene(next_scene_name);
-            }
-            else
+            if (!bang)
             {
-                Debug.Log("No scene name");
+                StartCoroutine(Victory_Sequence());
+                bang = true;
             }
         }
     }
