59764fe6d809911244f0b8e8d91665549af22066
James Lillicrap <jamesplillicrap@gmail.com>
2020-09-29 02:56:49 -0400

    +Added Launcher and Arrows

---
+++ ConnectivityGame/Assets/Scripts/ArrowBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBehavior : MonoBehaviour
+{
+    public float life_time;
+
+    IEnumerator Die_Routine()
+    {
+        yield return new WaitForSeconds(life_time);
+        Destroy(gameObject);
+    }
+
+    public void Die() { StartCoroutine(Die_Routine()); }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Little One" || other.tag == "Big One")
+        {
+            other.GetComponent<CharacterMovement>().Die();
+        }
+        else if(other.tag == "Enemy" || other.tag == "BreakableBox")
+        {
+            Destroy(other.gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ ConnectivityGame/Assets/Scripts/LauncherBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherBehavior : ButtonParent
+{
+    public float delay;
+    public float launch_speed;
+    public Object Arrow;
+
+    private float max_delay;
+
+    private void Start()
+    {
+        max_delay = delay;
+        delay = 0.0f;
+    }
+
+    private void Update()
+    {
+        if(delay > 0.0f)
+        {
+            delay -= 1.0f * Time.deltaTime;
+        }
+    }
+
+    public override void Activate()
+    {
+        if (delay <= 0.0f)
+        {
+            GameObject arrow = (GameObject)Instantiate(Arrow, transform.position, transform.rotation);
+            arrow.GetComponent<Rigidbody2D>().velocity = transform.right * launch_speed;
+            arrow.GetComponent<ArrowBehavior>().Die();
+            delay = max_delay;
+        }
+    }
+}
