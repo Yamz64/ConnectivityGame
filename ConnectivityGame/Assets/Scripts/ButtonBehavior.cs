b421b2631993d01b214f38a1e3919f65eeb94a82
James Lillicrap <jamesplillicrap@gmail.com>
2020-09-29 00:27:49 -0400

    +Implemented Button Behavior added spawn limit to block spawner

---
+++ ConnectivityGame/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public bool in_trigger;
+
+    public Color color;
+    public GameObject affected;
+    private SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        rend.color = color;
+    }
+
+    private void Update()
+    {
+
+
+        if (affected != null)
+        {
+            affected.GetComponent<SpriteRenderer>().color = color;
+
+            if (in_trigger && Input.GetButtonDown("Action"))
+            {
+                affected.GetComponent<ButtonParent>().Activate();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Little One")
+        {
+            in_trigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Little One")
+        {
+            in_trigger = false;
+        }
+    }
+}
--- ConnectivityGame/Assets/Scripts/DispenserBehavior.cs
+++ ConnectivityGame/Assets/Scripts/DispenserBehavior.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class DispenserBehavior : ButtonParent
 {
+    public float spawn_amount;
     public bool toggle;
 
     public enum SpawnMode { BLOCK, BREAKABLEBLOCK, JUMPBLOCK };
     public SpawnMode mode;
 
     public Vector2 offset;
+    public List<GameObject> spawned;
     public List<Object> spawnables;                                 //Index 0 = bloc, Index 1 = breakableblock, Index 2 = JumpBlock
 
     private Animator anim;
@@ -25,18 +27,24 @@ public class DispenserBehavior : ButtonParent
         switch (mode) {
             case SpawnMode.BLOCK:
                 anim.SetInteger("SpawnState", 0);
-                Instantiate(spawnables[0], (Vector2)transform.position + offset, transform.rotation);
+                spawned.Add((GameObject)Instantiate(spawnables[0], (Vector2)transform.position + offset, transform.rotation));
                 break;
             case SpawnMode.BREAKABLEBLOCK:
                 anim.SetInteger("SpawnState", 1);
-                Instantiate(spawnables[1], (Vector2)transform.position + offset, transform.rotation);
+                spawned.Add((GameObject)Instantiate(spawnables[1], (Vector2)transform.position + offset, transform.rotation));
                 break;
             case SpawnMode.JUMPBLOCK:
                 anim.SetInteger("SpawnState", 2);
-                Instantiate(spawnables[2], (Vector2)transform.position + offset, transform.rotation);
+                spawned.Add((GameObject)Instantiate(spawnables[2], (Vector2)transform.position + offset, transform.rotation));
                 break;
             default:
                 break;
+        }
+
+        if(spawned.Count > spawn_amount)
+        {
+            Destroy(spawned[0].gameObject);
+            spawned.RemoveAt(0);
         }
     }
 }
